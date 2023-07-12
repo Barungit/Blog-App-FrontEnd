d4bc88336b671fa387c5984196628b56f42b0de6
Barungit <barunkr0801@gmail.com>
2023-07-12 20:33:01 +0530

    Added Badge on categories

--- src/Components/CategorySideMenu.jsx
+++ src/Components/CategorySideMenu.jsx
@@ -1,40 +1,61 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { ListGroup, ListGroupItem, NavLink } from 'reactstrap'
-import { loadAllCategories } from '../services/category-service'
-import { toast } from 'react-toastify'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Badge, ListGroup, ListGroupItem } from "reactstrap";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+import { loadAllCategories } from "../services/category-service";
+import { loadPostbyCategory } from "../services/post-service";
 
 function CategorySideMenu() {
+  const [categories, setCategories] = useState([]);
+  const [batchNumbers, setBatchNumbers] = useState({});
 
-    const [categories, SetCategories]=useState([])
+  useEffect(() => {
+    loadAllCategories()
+      .then((data) => {
+        console.log(data);
+        setCategories([...data]);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Error in loading categories!");
+      });
+  }, []);
 
-    useEffect(() => {
-        loadAllCategories().then(data => {
-            console.log(data);
-            SetCategories([...data])
-        }).catch(error => {
-            console.log(error);
-            toast.error("Error in loading categories!");
-        })
-    },[])
+  useEffect(() => {
+    const fetchBatchNumber = async (categoryId) => {
+      try {
+        const posts = await loadPostbyCategory(categoryId, 0, 10);
+        const batchNumber = posts.totalElements;
+        setBatchNumbers((prevBatchNumbers) => ({
+          ...prevBatchNumbers,
+          [categoryId]: batchNumber,
+        }));
+      } catch (error) {
+        console.log(error);
+        toast.error("Error in loading blog count!");
+      }
+    };
+
+    categories.forEach((cat) => {
+      fetchBatchNumber(cat.categoryId);
+    });
+  }, [categories]);
 
   return (
     <div>
-        <ListGroup>
-            <ListGroupItem action={true} tag={Link} to="/blogs">All Blogs</ListGroupItem>
+      <ListGroup>
+        <ListGroupItem action tag={Link} to="/blogs">
+          All Blogs
+        </ListGroupItem>
 
-        {categories && categories.map((cat,index) => {
-            return (
-                <ListGroupItem key={index} action={true} tag={Link} to={'/categories/'+ cat.categoryId}>
-                    {cat.categoryTitle}
-                </ListGroupItem>
-            )
-        })}
-        </ListGroup>
+        {categories.map((cat, index) => (
+          <ListGroupItem className="my-2 " key={index} action tag={Link} to={`/categories/${cat.categoryId}`}>
+            {cat.categoryTitle} <Badge className="bg-success" style={{float :"right"}}>{batchNumbers[cat.categoryId]}</Badge>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
     </div>
-  )
+  );
 }
 
-export default CategorySideMenu
+export default CategorySideMenu;
