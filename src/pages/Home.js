12484013388193d0819558e925525527d5a45709
Barungit <barunkr0801@gmail.com>
2023-08-04 20:45:22 +0530

    Homepage changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,9 @@ import Base from "../Components/Base";
 import NewFeed from "../Components/NewFeed";
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.css';
-import { faGoogle, faInstagram, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle, faInstagram, faLinkedin, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import './Carousel.css';
+import './Home.css';
 import pp1 from '../images/boy.jpg';
 import pp2 from '../images/girl.jpg';
 import pp3 from '../images/girl2.jpg';
@@ -30,7 +31,7 @@ import { loadAllPost } from "../services/post-service";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { BASE_URL } from "../services/helper";
-import { faAddressCard, faMailBulk, faMobileScreen, faPhone, faWalkieTalkie } from "@fortawesome/free-solid-svg-icons";
+import { faAddressCard, faMailBulk, faMapMarkerAlt, faMobileScreen, faPhone, faWalkieTalkie } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "../Components/Footer";
 
@@ -118,31 +119,85 @@ const Home = () => {
       </Col>
         </Row>
 
-      <Row>
-      <h1 className="mx-3" style={{ color: '#333', fontSize: '34px', fontWeight: 'bold', margin: '20px' }}>Our Bloggers:</h1>
-      <div className="container d-flex justify-content-around">
-  <div className="card shadow d-flex justify-content-center align-items-center" style={{ width: '180px', height: '180px', borderRadius: '50%', boxShadow: 'inset 0 0 10px white' }}>
-    <img src={pp1} className="card-img-top" alt="Image" style={{ height: '150px', width: '150px', borderRadius:'75px' }} />
-  </div>
-
-  <div className="card shadow d-flex justify-content-center align-items-center" style={{width: '180px', height: '180px', borderRadius: '50%', boxShadow: 'inset 0 0 10px white' }}>
-    <img src={pp4} className="card-img-top" alt="Image" style={{ height: '150px', width: '150px', borderRadius:'75px'}} />
-  </div>
-
-  <div className="card shadow d-flex justify-content-center align-items-center" style={{width: '180px', height: '180px', borderRadius: '50%', boxShadow: 'inset 0 0 10px white' }}>
-    <img src={pp3}  className="card-img-top" alt="Image" style={{  height: '150px', width: '150px', borderRadius:'75px' }} />
-  </div>
-
-  <div className="card shadow d-flex justify-content-center align-items-center" style={{ width: '180px', height: '180px', borderRadius: '50%', boxShadow: 'inset 0 0 10px white' }}>
-    <img src={pp2}  className="card-img-top" alt="Image" style={{ height: '150px', width: '150px', borderRadius:'75px' }} />
-  </div>
-
-  <div className="card shadow d-flex justify-content-center align-items-center" style={{width: '180px', height: '180px', borderRadius: '50%', boxShadow: 'inset 0 0 10px white' }}>
-    <img src={pp5} className="card-img-top" alt="Image" style={{ height: '150px', width: '150px', borderRadius:'75px'}} />
-  </div>
-</div>
-
-      </Row>
+          {/* OUR BLOGGERS */}
+          <div className="container d-flex justify-content-around">
+          <div class="container">
+            <div class="text-center pb-2">
+                <p class="section-title px-5">
+                  
+                  <span class="px-2 ">Our Contributors</span></p>
+                <h1 class="mb-4">Meet Our Bloggers</h1>
+            </div>
+            <div class="row" >
+                <div class="col-md-6 col-lg-3 text-center team mb-5">
+                    <div class="position-relative overflow-hidden mb-4 " style={{borderRadius: '50%'}} >
+                    {/* <img src={pp1} className="card-img-top img-fluid w-100" alt="Image" style={{ height: '150px', width: '150px', borderRadius:'75px' }} /> */}
+                        <img class="img-fluid w-100" src={pp1} alt="" style={{height: '250px', width: '150px'}} />
+                        <div
+                            class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faTwitter} /></a>
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faLinkedin} /></a>
+                            <a class="btn btn-outline-light text-center px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faInstagram} /></a>
+                        </div>
+                    </div>
+                    <h4>Julia Smith</h4>
+                    <i>Music Teacher</i>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center team mb-5">
+                    <div class="position-relative overflow-hidden mb-4" style={{borderRadius: '50%'}}>
+                        <img class="img-fluid w-100" src={pp2} alt="" />
+                        <div
+                            class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faTwitter} /></a>
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faLinkedin} /></a>
+                            <a class="btn btn-outline-light text-center px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faInstagram} /></a>
+                        </div>
+                    </div>
+                    <h4>Julia Smith</h4>
+                    <i>Music Teacher</i>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center team mb-5">
+                    <div class="position-relative overflow-hidden mb-4" style={{borderRadius: '50%'}}>
+                        <img class="img-fluid w-100" src={pp3} alt=""  style={{height: '250px', width: '150px'}} />
+                        <div
+                            class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faTwitter} /></a>
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faLinkedin} /></a>
+                            <a class="btn btn-outline-light text-center px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faInstagram} /></a>
+                        </div>
+                    </div>
+                    <h4>Julia Smith</h4>
+                    <i>Music Teacher</i>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center team mb-5">
+                    <div class="position-relative overflow-hidden mb-4" style={{borderRadius: '50%'}}>
+                        <img class="img-fluid w-100" src={pp4} alt=""  style={{height: '250px', width: '150px'}}/>
+                        <div
+                            class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faTwitter} /></a>
+                            <a class="btn btn-outline-light text-center mr-2 px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faLinkedin} /></a>
+                            <a class="btn btn-outline-light text-center px-0" style={{width: '38px', height: '38px'}}
+                                href="#"><FontAwesomeIcon  icon={faInstagram} /></a>
+                        </div>
+                    </div>
+                    <h4>Julia Smith</h4>
+                    <i>Music Teacher</i>
+                </div>
+              </div>
+      
+          </div>
+     </div>
        
     </Base>
   );
